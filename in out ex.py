92559bd5fd5d6a398ee164b7d1776891256c972e
sakthi <156151745+Sakthi1030@users.noreply.github.com>
2024-02-07 18:51:11 +0530

    Add files via upload

---
+++ in out ex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=[]
+for i in range(5):
+    val = float(input("enter:"))
+    a.append(val)
+print(a)
+
+
+# In[1]:
+
+
+with open("sample-1.txt", "r") as fp:
+    lines = fp.readlines()
+with open("new_file.txt", "w") as fp:
+    count = 0
+    for line in lines: 
+        if count == 4:
+            count += 1
+            continue
+        else:
+            fp.write(line)
+        count += 1
+
